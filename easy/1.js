a2c6e0d950ae5466bba0312e96a47a48d2bb3628
zejustinn <joselsjustin@gmail.com>
2023-04-06 14:15:53 -0300

    refactor: standardize language and name of problem
    Refactor TwoSumTest.ts file to 1.js

--- easy/1.js
+++ easy/1.js
@@ -1,10 +1,12 @@
+const assert = require('node:assert');
+
 /** 
   Given an array of integers nums and an integer target, return indices of the two numbers such that
   they add up to target. You may assume that each input would have exactly one solution, and you may
   not use the same element twice. You can return the answer in any order.
 */
-export default function twoSum(nums: number[], target: number): number[] {
-  const result: number[] = []
+const twoSum = (nums, target) => {
+  const result = []
 
   for (let i = 0; i < nums.length; i++) {
     for(let j = i + 1; j < nums.length; j++) {
@@ -19,3 +21,7 @@ export default function twoSum(nums: number[], target: number): number[] {
 
   return result
 }
+
+assert.deepEqual(twoSum([2, 7, 11, 15], 9), [0, 1]);
+assert.deepEqual(twoSum([3, 2, 4], 6), [1, 2]);
+assert.deepEqual(twoSum([3, 3], 6), [0, 1]);
